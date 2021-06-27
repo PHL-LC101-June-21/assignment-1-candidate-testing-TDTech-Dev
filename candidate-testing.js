d953c51d09f7409589ad56d5ec7c688608cac2c9
TDTech <85696713+TDTech-Dev@users.noreply.github.com>
2021-06-27 20:50:40 +0000

    Final Version

--- candidate-testing.js
+++ candidate-testing.js
@@ -26,37 +26,38 @@ const candidateAnswers = [];
 
 
 function askForName() {
-  candidateName = input.question("Name: ")
-  console.log("Greetings "+ candidateName) 
+  candidateName = input.question("Candidate Name: ");
+  console.log("Greetings " + candidateName);
 }
 
 function askQuestion() {
-  for(let i = 0; i < questions.length; i += 1){
-    const candidateAnswer = input.question((i + 1) + ") " +  questions[i])
-    candidateAnswers.push(candidateAnswer)
+  for (let i = 0; i < questions.length; i += 1) {
+    const candidateAnswer = input.question(('\n') + (i + 1) + ") " + questions[i])
+    candidateAnswers.push(candidateAnswer);
+    console.log("Your Answer: " + candidateAnswers[i]);
+    console.log("Correct Answer: " + correctAnswers[i]);
+
   }
 }
 
 function gradeQuiz(candidateAnswers) {
   let correctCount = 0
 
-  for(let i = 0; i < questions.length; i += 1){
-    console.log("Your Answer: "+ candidateAnswers[i]);
-    console.log("Correct Answer: "+ correctAnswers[i]);
-    if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
+  for (let i = 0; i < questions.length; i += 1) {
+    if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()) {
       correctCount += 1
     }
   }
 
-  const grade = (correctCount)/(questions.length)*100;
-  console.log(grade + "% out of 100%")
+  const grade = (correctCount) / (questions.length) * 100;
+  console.log(('\n') + ">>> Overall Grade: " + grade + "% out of 100% <<<")
   if (grade >= 80) {
-    console.log(">>>Status:Passed<<<")
-    } else {
-      console.log(">>>Status:Failed<<<")
-    }
-    
-  
+    console.log(">>> Status: Passed <<<")
+  } else {
+    console.log(">>> Status: Failed <<<")
+  }
+
+
   return grade;
 }
 
